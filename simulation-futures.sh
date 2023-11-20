72ad22a979f05f0609acca00e86ee235990a3c06
Josh Errickson <josh@errickson.net>
2023-11-20 14:29:24 -0500

    Adding slurm options

--- simulation-futures.sh
+++ simulation-futures.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+#SBATCH --job-name=simulation_futures
+#SBATCH --mail-user=jerrick@umich.edu
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --cpus-per-task=8
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --mem-per-cpu=1000m
+#SBATCH --time=10:00
+#SBATCH --account=test
+#SBATCH --partition=standard
+#SBATCH --output=/home/%u/%x-%j.log
+
 Rscript simulation-futures.R
--- simulation.sh
+++ simulation.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+#SBATCH --job-name=simulation
+#SBATCH --mail-user=jerrick@umich.edu
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=1000m
+#SBATCH --time=10:00
+#SBATCH --account=cscar
+#SBATCH --partition=standard
+
 Rscript simulation.R
