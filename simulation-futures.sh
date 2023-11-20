fb1566fd08f7219e0980a24e6a3f5ebd983eb7bd
Josh Errickson <josh@errickson.net>
2023-11-20 15:07:45 -0500

    More updates to SLURM config

--- simulation-futures.sh
+++ simulation-futures.sh
@@ -3,13 +3,12 @@
 #SBATCH --job-name=simulation_futures
 #SBATCH --mail-user=jerrick@umich.edu
 #SBATCH --mail-type=BEGIN,END
-#SBATCH --cpus-per-task=8
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks=16
 #SBATCH --mem-per-cpu=1000m
 #SBATCH --time=10:00
-#SBATCH --account=test
+#SBATCH --account=cscar
 #SBATCH --partition=standard
-#SBATCH --output=/home/%u/%x-%j.log
 
+module load R
 Rscript simulation-futures.R
--- simulation.sh
+++ simulation.sh
@@ -3,12 +3,11 @@
 #SBATCH --job-name=simulation
 #SBATCH --mail-user=jerrick@umich.edu
 #SBATCH --mail-type=BEGIN,END
-#SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=1000m
 #SBATCH --time=10:00
 #SBATCH --account=cscar
 #SBATCH --partition=standard
 
+module load R
 Rscript simulation.R
