850dc22c0becb4993a11c593311821b4ca6d8a71
Josh Errickson <josh@errickson.net>
2024-11-20 22:03:28 -0500

    Tweak requested resources

--- simulation-futures.sh
+++ simulation-futures.sh
@@ -4,8 +4,8 @@
 #SBATCH --mail-user=jerrick@umich.edu
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --nodes=1
-#SBATCH --ntasks=16
-#SBATCH --mem-per-cpu=1000m
+#SBATCH --ntasks=8
+#SBATCH --mem-per-cpu=1g
 #SBATCH --time=10:00
 #SBATCH --account=stats506f24_class
 #SBATCH --partition=standard
--- simulation.sh
+++ simulation.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=jerrick@umich.edu
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --nodes=1
-#SBATCH --mem-per-cpu=1000m
+#SBATCH --mem-per-cpu=1g
 #SBATCH --time=10:00
 #SBATCH --account=stats506f24_class
 #SBATCH --partition=standard
